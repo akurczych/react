3ec859703fa73c22b2a95a55d7b1504f877a5ebe
akurczych <akurczych@gmail.com>
2023-02-16 00:43:59 +0000

    todo - v4 - persistently storing data

--- todo/src/App.js
+++ todo/src/App.js
@@ -10,11 +10,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Adam",
-      todoItems: [{ action: "Buy Flowers", done: false },
-                  { action: "Get Shoes", done: false },
-                  { action: "Collect Tickets", done: true },
-                  { action: "Call Joe", done: false }],
+      userName: "Andrzej",
+      todoItems: [],
       showCompleted: true
     }
   }
@@ -22,20 +19,35 @@ export default class App extends Component {
   createNewTodo = (task) => {
     if (!this.state.todoItems.find(item => item.action === task)) {
       this.setState({
-        todoItems: [...this.state.todoItems, { action: task, done: false }],
-      })
+        todoItems: [...this.state.todoItems, { action: task, done: false }]
+      },
+      () => localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   }
 
-  toggleTodo = (todo) => this.setState({ todoItems: 
-    this.state.todoItems.map(item => item.action === todo.action 
-        ? { ...item, done: !item.done } : item) });
+  toggleTodo = (todo) => {
+    this.setState({ todoItems: 
+      this.state.todoItems.map(item => item.action === todo.action 
+        ? { ...item, done: !item.done } : item) },
+        () => localStorage.setItem("todos", JSON.stringify(this.state)));
+  }
 
   todoTableRows = (doneValue) => this.state.todoItems
     .filter(item => item.done === doneValue)
     .map(item =>
       <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />)
-
+  
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+      ? JSON.parse(data)
+      : {
+        userName: "Andrzej",
+        todoItems: [],
+        showCompleted: true
+      })
+  }
+  
   render = () =>
     <div>
       <TodoBanner name={ this.state.userName } tasks={this.state.todoItems } />
