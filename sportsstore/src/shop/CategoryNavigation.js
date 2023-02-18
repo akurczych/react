cbf9dd8b57c5261e1f9bd2d000038765ebc1409a
akurczych <akurczych@gmail.com>
2023-02-18 23:17:28 +0000

    sportsstore-v0.1.1: improving category selection buttons

--- sportsstore/src/shop/CategoryNavigation.js
+++ sportsstore/src/shop/CategoryNavigation.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
 
     render() {
-        return <React.Fragment>
-            <Link className="btn btn-secondary btn-block" to={this.props.baseUrl}>All</Link>
-            {this.props.categories && this.props.categories.map(cat =>
-                <Link className="btn btn-secondary btn-block"
-                    key={ cat }
-                    to={ `${this.props.baseUrl}/${cat.toLowerCase()}` }>
+        return<React.Fragment>
+            <ToggleLink to={ this.props.baseUrl } exact={ true }>All</ToggleLink>
+            { this.props.categories && this.props.categories.map(cat =>
+                <ToggleLink key={ cat } to={ `${this.props.baseUrl}/${cat.toLowerCase()}` }>
                     { cat }
-                </Link>
+                </ToggleLink>
             )}
         </React.Fragment>
     }
