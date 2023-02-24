b0157f636ee6a8ba81d82fa975291c320ffa32c7
akurczych <akurczych@gmail.com>
2023-02-24 00:29:50 +0000

    hc-v3: separate components

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -25,27 +25,41 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text'></input>
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
 
     </div>
+  )
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text'></input>
+    </div>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   )
 }
 
