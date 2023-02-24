d2a9dc3a218e651db9dc35dfe10a43d8d1af608d
akurczych <akurczych@gmail.com>
2023-02-24 00:44:30 +0000

    hc-v4: arrow function

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -19,23 +19,20 @@ const list = [
   }
 ]
 
-function App() {
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <Search />
 
-      <Search />
+    <hr />
 
-      <hr />
+    <List />
 
-      <List />
+  </div>
+)
 
-    </div>
-  )
-}
-
-function Search() {
+const Search = () => {
   return (
     <div>
       <label htmlFor='search'>Search: </label>
@@ -44,23 +41,20 @@ function Search() {
   )
 }
 
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  )
-}
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+)
+
 
 export default App;
