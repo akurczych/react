6ddab702f8329b41e5d7d5366aba5c274e089ad6
akurczych <akurczych@gmail.com>
2023-03-02 23:26:02 +0000

    hc-v10: props nested deconstructing

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -59,14 +59,23 @@ const List = ({list}) => (
   </ul>
 )
 
-const Item = ({item}) => (
-  <li key={item.objectID}>
+const Item = ({
+  item: {
+    title,
+    url,
+    author,
+    num_comments,
+    points,
+    objectID
+  }
+}) => (
+  <li key={objectID}>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 )
 
