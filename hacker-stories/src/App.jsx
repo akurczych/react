21a2abe5a83057423f2d16357c8c8f27645708d4
akurczych <akurczych@gmail.com>
2023-03-12 23:40:09 +0000

    hc-v16:inline handlers in jsx

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-const useStorageState = (key, initialState) =>  {
+const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
   );
@@ -12,26 +12,33 @@ const useStorageState = (key, initialState) =>  {
   return [value, setValue];
 };
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 const App = () => {
 
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter((story) => item.objectID !== story.objectID);
+    setStories(newStories);
+  }
 
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
@@ -54,7 +61,7 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
 
     </div>
   )
@@ -68,29 +75,35 @@ const InputWithLabel = ({
   onInputChange }) => (
   <>
     <label htmlFor={id}>{label}</label>
-    &nbsp
+    &nbsp;
     <input id={id} type={type} value={value} onChange={onInputChange} ></input>
   </>
 )
 
-const List = ({list}) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
-    {list.map(({objectID, ...item}) => ( // Rest operator is used to deconstruct the objectID from the rest of the item object
-      <Item key={item.objectID} {...item} /> /* Spread operator is used to spread the rest of the item's key/value pairs into the Item component.
-                                                In this solution the objectID is not passed to the Item component. */
+    {list.map((item) => (
+      <Item
+        key={item.objectID}
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
     ))}
   </ul>
 )
 
-const Item = ({title, url, author, num_comments, points}) => (
+const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button>
+    </span>
   </li>
-)
+);
 
 export default App;
