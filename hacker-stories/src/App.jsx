c63669224536fb62fd3794f5e3f2224dee98d57a
akurczych <akurczych@gmail.com>
2023-03-14 23:56:04 +0000

    hc-v18: react conditional rendering

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -42,11 +42,16 @@ const useStorageState = (key, initialState) => {
 const App = () => {
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -75,8 +80,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
 
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   )
 }
