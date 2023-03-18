4f9d997ea6214913a1548307b395d26ce9e9d59c
akurczych <akurczych@gmail.com>
2023-03-18 00:24:49 +0000

    hs-v22:server-side search

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -49,15 +49,19 @@ const useStorageState = (key, initialState) => {
 
 const App = () => {
 
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -68,7 +72,7 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -77,14 +81,12 @@ const App = () => {
     });
   }
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-
   const handleSearch = (event) => {
     setSearchTerm(event.target.value)
   }
 
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  // const searchedStories = stories.data.filter((story) =>
+  //   story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
@@ -103,7 +105,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   )
